49c2bfdabe0c414e45828497d8372424867e15ee
Kirill Minenko <kirill.minenko@sperasoft.com>
2017-03-29 18:25:29 +0300

    Experiments with n-grams

---
+++ index.js
@@ -0,0 +1,24 @@
+const nGram = require('n-gram');
+
+let vocabulary = [
+    'ABAKAS',
+    'ABALONE',
+    'ABALONES',
+    'ABAMP',
+    'ABAMPERE',
+    'ABAMPERES',
+    'ABAMPS',
+    'ABANDON',
+    'ABANDONED',
+    'ABANDONER',
+    'ABANDONERS',
+    'ABANDONING',
+    'ABANDONMENT',
+    'ABANDONMENTS',
+];
+
+let word = 'ABANDANIN';
+
+
+console.log(nGram.bigram(vocabulary[5]));
+console.log(nGram.bigram(word));
