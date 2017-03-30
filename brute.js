e3c63d9c5b4954d1b04949ce69e14cd60e98f704
Kirill Minenko <kirill.minenko@sperasoft.com>
2017-03-30 17:24:05 +0300

    Experiments with n-grams #6

--- brute.js
+++ brute.js
@@ -1,85 +1,12 @@
 const fs = require('fs');
-const nGram = require('n-gram');
 const levenshtein = require('./levenshtein');
 const splitSentence = require('./sentence');
-
-function uniq(array) {
-    let seen = {};
-    return array.filter((item) => {
-        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
-    });
-}
-
-function wrap(word, n) {
-    "use strict";
-    if (n > 1 && n <= 2) {
-        return '_' + word + '_';
-    } else if (n <= 3) {
-        return '__' + word + '__';
-    } else if (n > 3) {
-        return '___' + word + '___';
-    } else if (n > 4) {
-        return '____' + word + '____';
-    } else if (n > 5) {
-        return '_____' + word + '_____';
-    }
-    return word;
-}
-
-function autoNGram(word) {
-    "use strict";
-    let ngCount = 3;
-
-    let vectors = nGram(ngCount)(wrap(word, ngCount));
-
-    if (word.length <= 6) {
-        vectors = vectors.concat(nGram(ngCount-1)(wrap(word, ngCount-1)))
-    }
-    if (word.length > 7) {
-        vectors = vectors.concat(nGram(ngCount+1)(wrap(word, ngCount+1)))
-    }
-    if (word.length > 8) {
-        //vectors = vectors.concat(nGram(ngCount+2)(wrap(word, ngCount+2)))
-    }
-    if (word.length > 9) {
-        //vectors = vectors.concat(nGram(ngCount+3)(wrap(word, ngCount+3)))
-    }
-    if (word.length > 11) {
-        //vectors = vectors.concat(nGram(ngCount+4)(wrap(word, ngCount+4)))
-    }
-
-    vectors.forEach((vector) => {
-        if (vector.length < 4 || vector.indexOf('_') >= 0) {
-            return;
-        }
-
-        for (let i = 1; i < vector.length-2; i++) {
-            vectors.push(vector.substr(0, i) + '_' + vector.substr(i + 1));
-        }
-
-        for (let i = 1; i < vector.length-2; i++) {
-            vectors.push(vector.substr(0, i) + vector.substr(i + 1));
-        }
-
-        for (let i = 0; i < vector.length-1; i++) {
-            if ('AEUIO'.split('').indexOf(vector.charAt(i)) >=0) {
-                vectors.push(vector.substr(0, i) + vector.substr(i + 1));
-            }
-        }
-    });
-
-    vectors = uniq(vectors);
-
-    //vector.push(word.length);
-    //vector.push(word.length-1);
-    //vector.push(word.length-2);
-
-    return vectors;
-}
+const autoNGram = require('./ngram').autoNGram;
+const threeGram = require('./ngram').threeGram;
 
 let ngCount = 2;
 console.time(' - Getting vocabulary');
-fs.readFile('./data/vocabulary.txt', {encoding: 'utf8'}, (err, data) => {
+fs.readFile('./data/vocabulary.txt', {encoding: 'utf8'}, (err, vocabulary) => {
     "use strict";
 
     if (err) {
@@ -90,70 +17,64 @@ fs.readFile('./data/vocabulary.txt', {encoding: 'utf8'}, (err, data) => {
     console.timeEnd(' - Getting vocabulary');
 
     console.time(' - Splitting vocabulary');
-    let words = data.split(/[\r\n]+/g);
+    let vocabularyWords = vocabulary.split(/[\r\n]+/g);
     console.timeEnd(' - Splitting vocabulary');
 
-    // limiting the number of words
-    // words = words.slice(1, 10);
+    console.log('number of words in vocabulary:', vocabularyWords.length);
 
-    console.log('number of words in vocabulary:', words.length);
-
-    let index = {};
+    let nGramIndex = {};
     let wordIndex = {};
     let wordFreqIndex = {};
 
-    console.time(' - Building freq vocabulary index');
-    words.forEach((word, i) => {
-        word = word.trim();
-
-        if (i%100===0) {console.log(`${i} of ${words.length}`)};
-        words.forEach((freqWord) => {
-            if (freqWord.indexOf(word) >= 0) {
-                wordFreqIndex[word] = wordFreqIndex[word] || 0;
-                wordFreqIndex[word]++;
-            }
-        });
-    });
-    console.timeEnd(' - Building freq vocabulary index');
+    // console.time(' - Building freq vocabulary index');
+    // vocabularyWords.forEach((word, i) => {
+    //     word = word.trim();
+    //
+    //     if (i%100===0) {console.log(`${i} of ${vocabularyWords.length}`)};
+    //     vocabularyWords.forEach((freqWord) => {
+    //         if (freqWord.indexOf(word) >= 0) {
+    //             wordFreqIndex[word] = wordFreqIndex[word] || 0;
+    //             wordFreqIndex[word]++;
+    //         }
+    //     });
+    // });
+    // console.timeEnd(' - Building freq vocabulary index');
 
     console.time(' - Building n-gram vocabulary index');
-    words.forEach((word) => {
-        word = word.trim();
+    vocabularyWords.forEach((vocabularyWord, vocabularyWordIndex) => {
+        if(!vocabularyWord) {
+            return;
+        }
 
-        wordIndex[word] = true;
+        wordIndex[vocabularyWord] = true;
 
-        autoNGram(word).forEach((ngram) => {
-            index[ngram] = index[ngram] || [];
-            index[ngram].push(word);
+        autoNGram(vocabularyWord).forEach((ngram) => {
+            nGramIndex[ngram] = nGramIndex[ngram] || [];
+            nGramIndex[ngram].push(vocabularyWordIndex + 1);
         });
     });
     console.timeEnd(' - Building n-gram vocabulary index');
 
     console.time(' - Getting input');
-    let subsctCount = 0;
+    let total = 0;
     fs.readFile('./data/example_input', {encoding: 'utf8'}, (err, data) => {
         if (err) {
             console.log('error', err);
             return err;
         }
-
         console.timeEnd(' - Getting input');
-        let words = splitSentence(data);
 
-        // testing
-        // words = ['accumsan', 'accumulations'];
+        let words = splitSentence(data).map(word => word.trim().toUpperCase());
 
-        console.time(' - Finding similar words for input');
+        console.time(' - Finding similar vocabularyWords for input');
         words.forEach((word) => {
-            word = word.trim().toUpperCase();
-
             if (word.length <= 1 || wordIndex[word]) {
                 console.log(`word: [${word}] exists`);
                 return;
             }
 
             // console.time(' - Building n-gram index for input');
-            let bestWordsIndex = {};
+            let bestWordsIndex = [];
 
             if (word.length > 5) {
                 ngCount = 3;
@@ -163,62 +84,42 @@ fs.readFile('./data/vocabulary.txt', {encoding: 'utf8'}, (err, data) => {
 
             let nGramsForWord = autoNGram(word);
             nGramsForWord.forEach((ngram) => {
-                if (index[ngram]) {
+                if (nGramIndex[ngram]) {
 
-                    //console.log(ngram, ' ',index[ngram].length);
-                    index[ngram].forEach((wordInResult) => {
-                        if (wordInResult && ((word.length+1) > wordInResult.length) && wordInResult.length > (word.length-1)) {
-                            bestWordsIndex[wordInResult] = bestWordsIndex[wordInResult] || 0;
-                            bestWordsIndex[wordInResult]++;
+                    nGramIndex[ngram].forEach((wordIndex) => {
+                        if (!bestWordsIndex[wordIndex]) {
+                            bestWordsIndex[wordIndex] = [i, 0, levenshtein(word, vocabularyWords[wordIndex-1])];
+                        } else {
+                            bestWordsIndex[wordIndex][1]++;
                         }
                     });
                 }
             });
 
+            let invertedIndex = [];
+            bestWordsIndex.forEach((wordIndex) => {
+                invertedIndex
+            });
+
             // console.timeEnd(' - Building n-gram index for input');
-
-            let sortable = [];
-            for (let item in bestWordsIndex) {
-                sortable.push([item, bestWordsIndex[item]]);
-            }
-
-            sortable = sortable.sort(function(a, b) {
+            let sortable = bestWordsIndex.sort(function(a, b) {
                 if (a[1] > b[1]) return -1;
                 if (a[1] < b[1]) return 1;
                 return 0;
             });
 
-            let bestWordsLD = [];
+            let bestWordInfo = sortable.slice(0, 1)[0];
+            let best = vocabularyWords[bestWordInfo[0] - 1];
+            let distance = bestWordInfo[2];
 
-            sortable.slice(0, 10).forEach(bw => {
-                bestWordsLD.push([bw[0], levenshtein(word, bw[0])]);
-            });
-
-            bestWordsLD = bestWordsLD.sort(function(a, b) {
-                if (a[1] > b[1]) return 1;
-                if (a[1] < b[1]) return -1;
-                return 0;
-            });
-
-            let best = sortable.slice(0, 1)[0][0];
-            let best2 = sortable.slice(1, 2)[0][0];
-            let best3 = sortable.slice(1, 3)[0][0];
-            let ld = levenshtein(word, best);
-            let ld2 = levenshtein(word, best2);
-            let ld3 = levenshtein(word, best3);
-
-            console.log(`word: [${word}] best: [${best}] similar: [${(sortable.slice(0, 10)).map(item => `${item[0]} (${item[1]})`).join(',')}]`);
+            console.log(`word: [${word}] best: [${best}]`);
             console.log('   - ngrams: ', JSON.stringify(nGramsForWord));
-            console.log('   - levenshtein: ', ld, ' ', ld2, ' ', ld3);
-            console.log('   - bestWordsLD: ', JSON.stringify(bestWordsLD));
-            // // console.log('   ', JSON.stringify(bestWordsIndex));
-            // console.log('   - best: ', JSON.stringify(bestWordsIndex[word.toUpperCase()]));
-            // console.log('   - top: ', JSON.stringify(sortable.slice(0, 10)));
-            // console.log('   ');
-            subsctCount += bestWordsLD[0][1];
-        });
-        console.timeEnd(' - Finding similar words for input');
+            console.log('   - similar: ', JSON.stringify(sortable));
+            console.log('   - distance: ', distance);
 
-        console.log('subst', subsctCount);
+        });
+        console.timeEnd(' - Finding similar vocabularyWords for input');
+
+        console.log('subst', total);
     });
 });
--- ngram.js
+++ ngram.js
@@ -1,30 +1,86 @@
-module.exports = function nGram(n) {
-    if (typeof n !== 'number' || isNaN(n) || n < 1 || n === Infinity) {
-        throw new Error('`' + n + '` is not a valid argument for n-gram');
+const nGram = require('n-gram');
+
+let uniq = function (array) {
+    let seen = {};
+    return array.filter((item) => {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
+};
+
+let wrap = function (word, n) {
+    "use strict";
+    if (n > 1 && n <= 2) {
+        return '_' + word + '_';
+    } else if (n <= 3) {
+        return '__' + word + '__';
+    } else if (n > 3) {
+        return '___' + word + '___';
+    } else if (n > 4) {
+        return '____' + word + '____';
+    } else if (n > 5) {
+        return '_____' + word + '_____';
+    }
+    return word;
+};
+
+let autoNGram = function(word) {
+    "use strict";
+    let ngCount = 3;
+
+    let vectors = nGram(ngCount)(wrap(word, ngCount));
+
+    if (word.length <= 6) {
+        vectors = vectors.concat(nGram(ngCount-1)(wrap(word, ngCount-1)))
+    }
+    if (word.length > 7) {
+        vectors = vectors.concat(nGram(ngCount+1)(wrap(word, ngCount+1)))
+    }
+    if (word.length > 8) {
+        //vectors = vectors.concat(nGram(ngCount+2)(wrap(word, ngCount+2)))
+    }
+    if (word.length > 9) {
+        //vectors = vectors.concat(nGram(ngCount+3)(wrap(word, ngCount+3)))
+    }
+    if (word.length > 11) {
+        //vectors = vectors.concat(nGram(ngCount+4)(wrap(word, ngCount+4)))
     }
 
-    return grams;
-
-    /* Create n-grams from a given value. */
-    function grams(value) {
-        var nGrams = [];
-        var index;
-
-        if (value === null || value === undefined) {
-            return nGrams;
+    vectors.forEach((vector) => {
+        if (vector.length < 4 || vector.indexOf('_') >= 0) {
+            return;
         }
 
-        value = String(value);
-        index = value.length - n + 1;
-
-        if (index < 1) {
-            return nGrams;
+        for (let i = 1; i < vector.length-2; i++) {
+            vectors.push(vector.substr(0, i) + '_' + vector.substr(i + 1));
         }
 
-        while (index--) {
-            nGrams[index] = value.substr(index, n);
+        for (let i = 1; i < vector.length-2; i++) {
+            vectors.push(vector.substr(0, i) + vector.substr(i + 1));
         }
 
-        return nGrams;
-    }
-}
+        for (let i = 0; i < vector.length-1; i++) {
+            if ('AEUIO'.split('').indexOf(vector.charAt(i)) >=0) {
+                vectors.push(vector.substr(0, i) + vector.substr(i + 1));
+            }
+        }
+    });
+
+    vectors = uniq(vectors);
+
+    //vector.push(word.length);
+    //vector.push(word.length-1);
+    //vector.push(word.length-2);
+
+    return vectors;
+};
+
+let threeGram = function(word) {
+    'use strict';
+    return nGram(3)(wrap(word, 3));
+};
+
+module.exports = {
+    wrap,
+    autoNGram,
+    threeGram
+};
