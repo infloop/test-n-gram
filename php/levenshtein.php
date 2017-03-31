3ae451ae7211725358ccb115d94595f8ec0e9921
infloop <infloop.gm@gmail.com>
2017-03-31 09:40:31 +0300

    Working #14

--- php/levenshtein.php
+++ php/levenshtein.php
@@ -1,8 +1,23 @@
 <?php
 
-$vocLoadTime = microtime(true);
+/**
+ * Very simple, but precise implementation of similar word search over vocabulary.
+ *
+ * There can be another solution that uses N-grams and nGramIndex to search similar words + Levenshtein distance,
+ * but it can be less precise.
+ *
+ * Speed measurements:
+ *
+ * 1. On my PC takes ~3100 ms to compute
+ * 2. On my Laptop it takes ~3800 ms to compute
+ *
+ * PHP 7.0 64-bit is used.
+ *
+ */
+
+$startupTime = microtime(true);
 $file = file_get_contents('./vocabulary.txt');
-// printf(" - Getting vocabulary %dms \n", (microtime(true) - $vocLoadTime)*1000);
+// printf(" - Getting vocabulary %dms \n", (microtime(true) - $startupTime)*1000);
 
 // $vocSplitTime = microtime(true);
 $vocabularyWords = preg_split("/[\r\n]+/", $file);
@@ -22,9 +37,8 @@ foreach($dirtyWords as $word) {
 // printf(" - Splitting input %dms \n", (microtime(true) - $inputSplitTime)*1000);
 
 $invertedWordsIndex = [];
-
-foreach($vocabularyWords as $index => $vocabularyWord) {
-    $invertedWordsIndex[$vocabularyWord] = $index;
+foreach($vocabularyWords as $vocabWord) {
+    $invertedWordsIndex[$vocabWord] = 1;
 }
 
 $total = 0;
@@ -34,31 +48,32 @@ foreach ($words as $word) {
     $minDistance = 10;
     // $mostSimilarWordIndex = -1;
 
+    // word exists in vocabulary - skipping
     if (array_key_exists($word, $invertedWordsIndex)) {
         continue;
     }
 
-    $vocabularyWord = '';
+    $vocabWord = '';
     for ($i = 0; $i < count($vocabularyWords) - 1; $i++ ) {
-        $vocabularyWord = $vocabularyWords[$i];
+        $vocabWord = $vocabularyWords[$i];
 
-        // minimum distance - skipping other steps
-        if ($minDistance === 1) {
+        // minimum distance - skipping other steps (== is faster then ===)
+        if ($minDistance == 1) {
             break;
         }
 
         // restriction for length difference for small-length words
-        if (strlen($word) <= 3 && strlen($vocabularyWord) - strlen($word) > 1) {
+        if (strlen($word) <= 3 && strlen($vocabWord) - strlen($word) > 1) {
             continue;
         }
 
         // restriction for length difference for medium-length words
-        if (strlen($word) >= 4 && strlen($vocabularyWord) - strlen($word) > 2) {
+        if (strlen($word) >= 4 && strlen($vocabWord) - strlen($word) > 2) {
             continue;
         }
 
-        // calculation of similarity/difference ratio
-        $distance = levenshtein($word, $vocabularyWord);
+        // calculation of levenshtein distance
+        $distance = levenshtein($word, $vocabWord);
 
         if ($distance < $minDistance) {
             // $mostSimilarWordIndex = $i;
@@ -70,7 +85,7 @@ foreach ($words as $word) {
     $total += $minDistance;
 }
 
-printf("%d \n", $total);
+printf("%d\n", $total);
 // printf(" - Calculating Levenshtein distances index %dms \n", (microtime(true) - $searchTime)*1000);
 // printf(" - Total number of substitutions %d \n", $total);
-// printf(" - Total time  %dms\n", (microtime(true) - $vocLoadTime)*1000);
+// printf(" - Total time  %dms\n", (microtime(true) - $startupTime)*1000);
